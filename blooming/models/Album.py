f5d4fca032408d3258c44485383167f9e06fb483
FBe9 <nereaoceja2003@gmail.com>
2023-01-19 08:55:37 +0100

    Cambios en el modelo de datos de Album

--- blooming/models/Album.py
+++ blooming/models/Album.py
@@ -8,10 +8,10 @@ class Album(models.Model):
     _name = 'blooming.album'
     
     #Attributes
-    album_id = fields.Integer()
-    name = fields.Char()
-    description = fields.Text()
-    #creator = fields.ManyToOne('res.users', string="Creator")
-    #users = fields.Many2Many('res.users', string="Users")
+    album_id = fields.Integer(string="Id")
+    name = fields.Char(string="Name", required=True, help="Name of the album")
+    description = fields.Text(string="Description")
+    creator = fields.Many2One('res.users', string="Creator", required=True, help="Login of the Album creator")
+    users = fields.Many2Many('res.users', string="Users")
     creationDate = fields.Date()
-    #contents = fields.Many2Many('blooming.content', string="Contents")
+    contents = fields.Many2Many('blooming.content', string="Contents")
