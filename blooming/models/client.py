b8d02f29b7da8f6b5f39573dd447a36ac4cd8b94
EnekoRuiz <enekoerre@gmail.com>
2023-01-20 14:11:15 +0100

    deleted ourUsers

--- blooming/models/client.py
+++ blooming/models/client.py
@@ -2,13 +2,12 @@ from odoo import api
 from odoo import fields
 from odoo import models
 
-class OurUsers(models.Model):
-    _inherit = 'res.users'
+class Client(models.Model):
     _name = 'res.users'
+    _inherit = 'res.users'
     
     sharedAlbums = fields.Many2many("blooming.album",
                                     string="SharedAlbums")
     createdAlbums = fields.Many2one("blooming.album", "album_id",
                                     string="CreatedAlbums")
-                                    
     plan = fields.Many2one("blooming.membersipplan", ondelete="set null", string="Plan", index=True)
