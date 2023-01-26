02d62ed330c9a9d18d95ecc062899dd930da177a
FBe9 <nereaoceja2003@gmail.com>
2023-01-26 13:18:21 +0100

    parte de album con la view empezada

--- blooming/models/Album.py
+++ blooming/models/Album.py
@@ -11,7 +11,7 @@ class Album(models.Model):
     album_id = fields.Integer(string="Id")
     name = fields.Char(string="Name", required=True, help="Name of the album")
     description = fields.Text(string="Description")
-    creator = fields.Many2One('res.users', string="Creator", required=True, help="Login of the Album creator")
+    creator = fields.Many2One('res.users', string="Creator", required=True)
     users = fields.Many2Many('res.users', string="Users")
     creationDate = fields.Date(string="Creation Date")
     contents = fields.Many2Many('blooming.content', string="Contents")
